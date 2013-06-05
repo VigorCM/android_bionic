6591597aca9c9e6feccede8cfe5d87fe84c8506e
David Eddlemon <neo@TheMatrix.com>
2013-06-05 23:09:28 +0400

    Linaro-Add ARMv7 optimized string handling routines

--- libc/Android.mk
+++ libc/Android.mk
@@ -185,7 +185,6 @@ libc_common_src_files := \
 	string/strcasecmp.c \
 	string/strcasestr.c \
 	string/strcat.c \
-	string/strchr.c \
 	string/strcoll.c \
 	string/strcspn.c \
 	string/strdup.c \
@@ -382,20 +381,10 @@ libc_common_src_files += \
 	arch-arm/bionic/memset.S \
 	arch-arm/bionic/setjmp.S \
 	arch-arm/bionic/sigsetjmp.S \
-	arch-arm/bionic/strcpy.S \
 	arch-arm/bionic/strcmp.S \
 	arch-arm/bionic/syscall.S \
 	string/strncmp.c \
 	unistd/socketcalls.c
-
-# String routines optimized for ARMv7
-ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
-libc_common_src_files += arch-arm/bionic/memchr.S
-libc_common_src_files += arch-arm/bionic/strlen-armv7.S
-else
-libc_common_src_files += string/memchr.c
-libc_common_src_files += arch-arm/bionic/strlen.c.arm
-endif
 
 # We have a special memcpy for A15 currently
 ifeq ($(TARGET_ARCH_VARIANT_CPU),cortex-a15)
@@ -406,7 +395,7 @@ endif
 
 # Check if we want a neonized version of memmove instead of the
 # current ARM version
-ifeq ($(ARCH_ARM_HAVE_NEON),true)
+ifeq ($(ARCH_ARM_HAVE_ARMV7A)-$(ARCH_ARM_HAVE_NEON),true-true)
  libc_common_src_files += \
 	arch-arm/bionic/memmove.S
  else # Other ARM
@@ -446,6 +435,37 @@ libc_arch_static_src_files := \
 
 libc_arch_dynamic_src_files := \
 	arch-arm/bionic/exidx_dynamic.c
+
+ifeq ($(ARCH_ARM_HAVE_ARMV7A),true)
+  ifneq ($(IS_ARMV7A_QCOM),true)
+  libc_common_src_files += \
+    arch-arm/bionic/memchr.S \
+    arch-arm/bionic/armv7/strchr.S \
+    arch-arm/bionic/armv7/strcpy.c \
+    arch-arm/bionic/armv7/strlen.S
+  endif
+else
+libc_common_src_files += \
+  string/memchr.c \
+  string/strchr.c \
+  arch-arm/bionic/strcpy.S \
+  arch-arm/bionic/strlen.c.arm
+endif
+
+# Don't use linaro string routines on QCOM chipsets
+ifeq ($(ARCH_ARM_HAVE_ARMV7A)-$(IS_ARMV7A_QCOM),true-true)
+libc_common_src_files += \
+  arch-arm/bionic/memchr.S \
+  string/strchr.c \
+  arch-arm/bionic/strcpy.S \
+  arch-arm/bionic/strlen.c.arm
+endif
+else # arm
+
+libc_common_src_files += \
+  string/memchr.c \
+  string/strchr.c
+
 endif # arm
 
 ifeq ($(TARGET_ARCH),x86)
@@ -593,8 +613,6 @@ ifeq ($(TARGET_ARCH),arm)
   ifeq ($(ARCH_ARM_USE_NON_NEON_MEMCPY),true)
     libc_common_cflags += -DARCH_ARM_USE_NON_NEON_MEMCPY
   endif
-
-
   ifeq ($(ARCH_ARM_HAVE_NEON_UNALIGNED_ACCESS),true)
     libc_common_cflags += -DNEON_UNALIGNED_ACCESS
   endif
